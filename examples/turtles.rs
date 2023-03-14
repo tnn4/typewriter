682e63ba8b55fb9d350ec441a50c3127e592d7ea
tnn4 <tnn130130@gmail.com>
2023-03-14 17:55:44 -0500

    improve example

--- examples/turtles.rs
+++ examples/turtles.rs
@@ -10,11 +10,12 @@ fn main() {
 
     terminal::term::type_text("Could you tell me what the world sits upon?\n");
 
-    terminal::term::type_color("A turtle.\n".green());
+    for i in 0..10 {
+        terminal::term::type_text_colored("red","A turtle");
+        terminal::term::type_text("And that sits upon?\n");
+    }
+    terminal::term::type_text_colored("red", "A TURTLE! THERE ARE TURTLES ALL THE WAY DOWN");
 
-    "this is blue".blue();
-
-    terminal::term::type_text_colored("red","some stringggggggggggggggggggggggggggggggggg");
     terminal::term::type_text_colored("blue","some stringggggggggggggggggggggggggggggggggg");
     terminal::term::type_text_colored("green","some stringggggggggggggggggggggggggggggggggg");
 
